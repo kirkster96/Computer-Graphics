f3f5eb7abec4822a478ccd8f76be9d33b46aced6
Cameron Kirk <kirkster96@gmail.com>
2019-09-08 23:28:26 -0700

    This is a project created using Visual Studio Community and written in C#. The program will use the constants entered and calculate the julia set on the complex plane. When a pixel is found to converge, it is colored blue. The threshold used is 20.
    Signed-off-by: Cameron Kirk <kirkster96@gmail.com>

---
+++ Julia_sets/Julia_sets/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Julia_sets
+{
+    partial class juliaSetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.playDefault = new System.Windows.Forms.Button();
+            this.userCoeff = new System.Windows.Forms.Button();
+            this.c0label = new System.Windows.Forms.Label();
+            this.c1Label = new System.Windows.Forms.Label();
+            this.c0UserValue = new System.Windows.Forms.NumericUpDown();
+            this.c1UserValue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c0UserValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1UserValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(575, 20);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(600, 600);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            // 
+            // playDefault
+            // 
+            this.playDefault.Location = new System.Drawing.Point(70, 20);
+            this.playDefault.Name = "playDefault";
+            this.playDefault.Size = new System.Drawing.Size(400, 120);
+            this.playDefault.TabIndex = 1;
+            this.playDefault.Text = "Play Default";
+            this.playDefault.UseVisualStyleBackColor = true;
+            this.playDefault.Click += new System.EventHandler(this.runDefaultValue);
+            // 
+            // userCoeff
+            // 
+            this.userCoeff.Location = new System.Drawing.Point(70, 160);
+            this.userCoeff.Name = "userCoeff";
+            this.userCoeff.Size = new System.Drawing.Size(400, 120);
+            this.userCoeff.TabIndex = 2;
+            this.userCoeff.Text = "User\'s Coefficients";
+            this.userCoeff.UseVisualStyleBackColor = true;
+            this.userCoeff.Click += new System.EventHandler(this.UserCoeff_Click);
+            // 
+            // c0label
+            // 
+            this.c0label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c0label.Location = new System.Drawing.Point(68, 301);
+            this.c0label.Name = "c0label";
+            this.c0label.Size = new System.Drawing.Size(148, 80);
+            this.c0label.TabIndex = 3;
+            this.c0label.Text = "c0";
+            this.c0label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c1Label
+            // 
+            this.c1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1Label.Location = new System.Drawing.Point(68, 395);
+            this.c1Label.Name = "c1Label";
+            this.c1Label.Size = new System.Drawing.Size(148, 80);
+            this.c1Label.TabIndex = 4;
+            this.c1Label.Text = "c1";
+            this.c1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c0UserValue
+            // 
+            this.c0UserValue.DecimalPlaces = 2;
+            this.c0UserValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c0UserValue.Location = new System.Drawing.Point(280, 301);
+            this.c0UserValue.Name = "c0UserValue";
+            this.c0UserValue.Size = new System.Drawing.Size(190, 75);
+            this.c0UserValue.TabIndex = 5;
+            // 
+            // c1UserValue
+            // 
+            this.c1UserValue.DecimalPlaces = 2;
+            this.c1UserValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1UserValue.Location = new System.Drawing.Point(280, 395);
+            this.c1UserValue.Name = "c1UserValue";
+            this.c1UserValue.Size = new System.Drawing.Size(190, 75);
+            this.c1UserValue.TabIndex = 6;
+            // 
+            // juliaSetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 642);
+            this.Controls.Add(this.c1UserValue);
+            this.Controls.Add(this.c0UserValue);
+            this.Controls.Add(this.c1Label);
+            this.Controls.Add(this.c0label);
+            this.Controls.Add(this.userCoeff);
+            this.Controls.Add(this.playDefault);
+            this.Controls.Add(this.picCanvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "juliaSetForm";
+            this.Text = "Julia Set";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c0UserValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1UserValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Button playDefault;
+        private System.Windows.Forms.Button userCoeff;
+        private System.Windows.Forms.Label c0label;
+        private System.Windows.Forms.Label c1Label;
+        private System.Windows.Forms.NumericUpDown c0UserValue;
+        private System.Windows.Forms.NumericUpDown c1UserValue;
+    }
+}
+
---
+++ Julia_sets/Julia_sets/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Julia_sets
+{
+    public partial class juliaSetForm : Form
+    {
+        public juliaSetForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void UserCoeff_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(picCanvas.ClientSize.Width, picCanvas.ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                g.Clear(Color.Red);
+
+                int px, py,threshold;
+                int i,j,k;
+                double ax, ay, bx, by, cx, cy;
+                decimal czero, cone;
+                bx = 0;
+                by = 0;
+                px = 0;
+                py = 0;
+                czero = c0UserValue.Value;
+                cone = c1UserValue.Value;
+
+                //starting point run
+                threshold = 20;
+
+                    
+                //begin loop
+                //iterate through every pixel
+                for(i=0;i<600;i++)
+                {
+                    for(j=0; j<600;j++)
+                    {
+                        ax = -2.0 + i * (4.0 / picCanvas.ClientSize.Width);
+                        ay = -2.0 + j * (4.0 / picCanvas.ClientSize.Height);
+
+                        //try 4096 iterations
+                        for (k = 0; k < 4096; k++)
+                        {
+                            bx = (ax * ax) - (ay * ay) + Decimal.ToDouble(czero);
+                            by = 2 * ax * ay + Decimal.ToDouble(cone);
+
+                            //check if converge and paint the pixel blue
+                            if (((bx * bx) + (by * by) < threshold) && bx > -2 && bx < 2 && by > -2 && by < 2)
+                            {
+                                px = Convert.ToInt32((bx + 2) * (picCanvas.ClientSize.Width / 4));
+                                py = Convert.ToInt32((by + 2) * (picCanvas.ClientSize.Height / 4));
+                                if (px < picCanvas.ClientSize.Width && py < picCanvas.ClientSize.Height)
+                                    bm.SetPixel(px, py, Color.Blue);
+                            }
+
+                            //break loop if diverge >> threshold                           
+                            if (((bx * bx) + (by * by) > threshold))
+                                break;
+                            
+                            //move forward
+                            ax = bx;
+                            ay = by;
+                        }
+                    }
+                }
+                    picCanvas.Image = bm;
+            }
+        }
+
+        private void runDefaultValue(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(picCanvas.ClientSize.Width, picCanvas.ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                g.Clear(Color.Red);
+
+                int px, py, threshold;
+                int i, j, k;
+                double ax, ay, bx, by, cx, cy;
+                decimal czero, cone;
+                bx = 0;
+                by = 0;
+                px = 0;
+                py = 0;
+                czero = 0;
+                cone = 1;
+
+                //starting point run
+                threshold = 20;
+
+
+                //begin loop
+                //iterate through every pixel
+                for (i = 0; i < 600; i++)
+                {
+                    for (j = 0; j < 600; j++)
+                    {
+                        ax = -2.0 + i * (4.0 / picCanvas.ClientSize.Width);
+                        ay = -2.0 + j * (4.0 / picCanvas.ClientSize.Height);
+
+                        //try 4096 iterations
+                        for (k = 0; k < 4096; k++)
+                        {
+                            bx = (ax * ax) - (ay * ay) + Decimal.ToDouble(czero);
+                            by = 2 * ax * ay + Decimal.ToDouble(cone);
+
+                            //check if converge and paint the pixel blue
+                            if (((bx * bx) + (by * by) < threshold) && bx > -2 && bx < 2 && by > -2 && by < 2)
+                            {
+                                px = Convert.ToInt32((bx + 2) * (picCanvas.ClientSize.Width / 4));
+                                py = Convert.ToInt32((by + 2) * (picCanvas.ClientSize.Height / 4));
+                                if (px < picCanvas.ClientSize.Width && py < picCanvas.ClientSize.Height)
+                                    bm.SetPixel(px, py, Color.Blue);
+                            }
+
+                            //break loop if diverge >> threshold                           
+                            if (((bx * bx) + (by * by) > threshold))
+                                break;
+
+                            //move forward
+                            ax = bx;
+                            ay = by;
+                        }
+                    }
+                }
+                picCanvas.Image = bm;
+            }
+        }
+
+    }
+}
